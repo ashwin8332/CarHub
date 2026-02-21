2ba7866a87de5fdbbe2b3e2d0d79885ea1f827aa
Ashwin Vincent <anshilashwin80@gmail.com>
2026-02-21 05:51:38 +0000

    Add static folder with Git LFS

---
+++ static/theme-fixer.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:afa84465e2f031c740d3563dc2f04cd613f1de92ddffa7782f9a54f55c385e87
+size 4460
---
+++ static/theme-toggle.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:720e99427bc809a7472fec79e3067e75da2d135a27148b12f07e6d388d55368c
+size 5946
